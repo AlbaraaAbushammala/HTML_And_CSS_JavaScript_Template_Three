f4deadf7958e3187fe465d37db0bc22a4313270c
AlbaraaAbushammala <albaraa_1991@MBP-Bero.fritz.box>
2022-09-03 15:13:24 +0200

    Upload The Design

---
+++ main.js
@@ -0,0 +1,25 @@
+
+
+let countDownDate = new Date("Dec 10, 2022 23:59:59").getTime();
+// console.log(countDownDate);
+
+let counter = setInterval(() => {
+    let dateNow = new Date().getTime();
+    let dateDiff = countDownDate - dateNow;
+    let days = Math.floor(dateDiff / (1000 * 60 * 60 * 24));               
+    document.querySelector(".days").innerHTML = days < 10 ? `0${days}` : days;;
+
+    let hours = Math.floor((dateDiff % (1000 * 60 * 60 * 24)) / (1000 *  60 *  60) );  
+    document.querySelector(".hours").innerHTML = hours < 10 ? `0${hours}` : hours;;    
+
+    let minutes = Math.floor((dateDiff % (1000 * 60 * 60)) / (1000 * 60 ));
+    document.querySelector(".minutes").innerHTML = minutes < 10 ? `0${minutes}` : minutes;;    
+    
+    let seconds = Math.floor((dateDiff % (1000 * 60 )) / 1000);
+    document.querySelector(".seconds").innerHTML = seconds < 10 ? `0${seconds}` : seconds; 
+
+
+
+
+
+}, 1000);
